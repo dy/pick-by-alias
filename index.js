7675530ac70eece91bb9c9ad6b80210acd0628c2
Dmitry <df.creative@gmail.com>
2017-09-16 23:10:12 -0400

    Add caching

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict'
 
+var CACHE = {}
+
 module.exports = function pick (src, props) {
 	var result = {}
 
 	for (var prop in props) {
 		var aliases = props[prop]
-		if (typeof aliases === 'string') aliases = aliases.split(/\s*,\s*|\s+/)
+
+		if (CACHE[aliases]) aliases = CACHE[aliases]
+		else if (typeof aliases === 'string') {
+			aliases = CACHE[aliases] = aliases.split(/\s*,\s*|\s+/)
+		}
 
 		if (Array.isArray(aliases)) {
 			for (var i = 0; i < aliases.length; i++) {
